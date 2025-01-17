b95020bbdb516a353e4ee3b90fc436adef395f0e
Sirius <sirius@example.com>
2025-01-17 23:14:29 +0900

    修改主页路由，打印POPER_CUSTOM_VARIABLE环境变量值

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    $message = env('POPER_CUSTOM_VARIABLE', 'NULL'); // 获取环境变量，若不存在用 NULL 做默认值。
+    return "Hello from POPER TEST! POPER_CUSTOM_VARIABLE -> " . $message;
 });

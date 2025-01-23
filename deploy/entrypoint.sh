203b72f57ba75ac8a6e7556050d98a5bb073fc9c
Sirius <sirius@example.com>
2025-01-24 02:57:15 +0900

    update entrypoint script & vector config

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -2,8 +2,14 @@
 set -e
 
 mkdir -p /var/log/exported
-chown www-data:www-data /var/log/exported
+chown www-data:www-data /var/log/exported 
 chmod 775 /var/log/exported
+
+mkdir -p /opt/aws/appconfig/
+chown -R www-data:www-data /opt/aws/appconfig/
+chmod -R 775 /opt/aws/appconfig/
+
+chown -R www-data:www-data /var/www/html/storage/logs/
 
 # 复制 vector 配置文件
 if [ ! -f /var/log/exported/vector.toml ]; then
